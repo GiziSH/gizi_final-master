da684c5b3346879f7aa318cb4c7f998ed6344336
suhyun <ho03113@gmail.com>
2018-08-18 14:21:57 +0900

    180818

--- app/src/main/java/com/dsk/gizi_final/MainActivity.java
+++ app/src/main/java/com/dsk/gizi_final/MainActivity.java
@@ -1,14 +1,118 @@
 package com.dsk.gizi_final;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by suhyun on 2018-03-30.
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private final int FRAGMENT1 = 1;
+    private final int FRAGMENT2 = 2;
+    private final int FRAGMENT3 = 3;
+    private final int FRAGMENT4 = 4;
+    private final int FRAGMENT5 = 5;
+
+    private Button bt_tab1, bt_tab2,bt_tab3,bt_tab4,bt_tab5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bt_tab1 = (Button)findViewById(R.id.bt_tab1);
+        bt_tab2 = (Button)findViewById(R.id.bt_tab2);
+        bt_tab3 = (Button)findViewById(R.id.bt_tab3);
+        bt_tab4 = (Button)findViewById(R.id.bt_tab4);
+        bt_tab5 = (Button)findViewById(R.id.bt_tab5);
+
+        bt_tab1.setOnClickListener(this);
+        bt_tab2.setOnClickListener(this);
+        bt_tab3.setOnClickListener(this);
+        bt_tab4.setOnClickListener(this);
+        bt_tab5.setOnClickListener(this);
+
+        callFragment(FRAGMENT1);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_tab1 :
+                // '버튼1' 클릭 시 '프래그먼트1' 호출
+                callFragment(FRAGMENT1);
+                break;
+
+            case R.id.bt_tab2 :
+                // '버튼1' 클릭 시 '프래그먼트1' 호출
+                callFragment(FRAGMENT2);
+                break;
+
+            case R.id.bt_tab3 :
+                // '버튼3' 클릭 시 '프래그먼트3' 호출
+                callFragment(FRAGMENT3);
+                break;
+
+            case R.id.bt_tab4 :
+                // '버튼4' 클릭 시 '프래그먼트4' 호출
+                callFragment(FRAGMENT4);
+                break;
+            case R.id.bt_tab5 :
+                // '버튼5' 클릭 시 '프래그먼트5' 호출
+                callFragment(FRAGMENT5);
+                break;
+
+        }
+    }
+
+    private void callFragment(int frament_no) {
+
+        // 프래그먼트 사용을 위해
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (frament_no) {
+            case 1:
+                // '프래그먼트1' 호출
+                Fragment1 fragment1 = new Fragment1();
+                transaction.replace(R.id.fragment_container, fragment1);
+                transaction.commit();
+                break;
+
+            case 2:
+                // '프래그먼트2' 호출
+                Fragment2 fragment2 = new Fragment2();
+                transaction.replace(R.id.fragment_container, fragment2);
+                transaction.commit();
+                break;
+            case 3:
+                // '프래그먼트3' 호출
+                fragment3_toiletstate f3t = new fragment3_toiletstate();
+                transaction.replace(R.id.fragment_container,f3t);
+                //Fragment3 fragment3 = new Fragment3();
+                //transaction.replace(R.id.fragment_container, fragment3);
+                transaction.commit();
+                break;
+
+            case 4:
+                // '프래그먼트4' 호출
+                Fragment4 fragment4 = new Fragment4();
+                transaction.replace(R.id.fragment_container, fragment4);
+                transaction.commit();
+                break;
+            case 5:
+                // '프래그먼트5' 호출
+                Fragment5 fragment5 = new Fragment5();
+                transaction.replace(R.id.fragment_container, fragment5);
+                transaction.commit();
+                break;
+
+        }
+
     }
 }
+
